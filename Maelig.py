109f8267d8cc01a1d4fcd38b0b83b58a2fc1342e
mae-leguevel <leguevelmaelig@gmail.com>
2024-10-29 16:41:49 +0100

    changement des données

--- Maelig.py
+++ Maelig.py
@@ -7,4 +7,28 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
+
+# %%
+from common import *
+
+# %%
+
+# %%
+
+
+
+
+# %%
+#remplacer les "--" par Na
+df.replace("--", np.nan, inplace=True)
+#sous tableau de donnee
+donnee = df.iloc[:,df.columns.get_loc('1980'):df.columns.get_loc('2021')]
+#remplacer objet par float
+for column in donnee.columns :
+    donnee[column] = pd.to_numeric(donnee[column],errors = 'coerce')
+
