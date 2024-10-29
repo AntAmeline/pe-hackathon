497a163c6f8658664ebdc5d0f4a97f3de864fa42
Alexandre Manso de Zuniga <alexandre.mansodz@gmail.com>
2024-10-29 14:55:02 +0100

    ajout de dossier de Alexandre

--- common.py
+++ common.py
@@ -1,8 +1,12 @@
 #fichier contenant les données communes à tout le monde
 import pandas as pd 
 import geopandas as gpd 
+import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('world-country-electricity.csv')
+
+np.sum(df.isna().to_numpy())
 
 
 
