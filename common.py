645c0b2b5c9ce1d66bd0abb88dad9d7e3b68ec5f
AntAmeline <antonin.ameline@etu.minesparis.psl.eu>
2024-10-29 17:01:43 +0100

    df VRAIMENT numérisée

--- common.py
+++ common.py
@@ -6,7 +6,8 @@ import geopandas as gpd
 df_orig = pd.read_csv('world-country-electricity.csv')
 #sans valeur fausse
 df_cleared=df_orig.dropna(how='all')
-df_cleared=df_cleared.apply(pd.to_numeric, errors='coerce')
+for column in df_cleared.columns[3:]:
+    df_cleared[column]=pd.to_numeric(df_cleared[column], errors='coerce')
 #réorganisation
 df_byfeature=df_cleared.drop('Region', axis=1)
 df_byfeature=df_byfeature.set_index('Features')
@@ -15,7 +16,7 @@ df_bycountry=df_bycountry.set_index('Country')
 df_cleared.dtypes
 
  #par région
-df_byregion=df_byregion.pivot_table(index=['Region','Features'],aggfunc='sum', fill_value=0)
+df_byregion=df_cleared.pivot_table(index=['Region','Features'],aggfunc='sum', fill_value=0)
 df_byregion
 
 
