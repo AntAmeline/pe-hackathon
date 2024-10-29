a3a2ec6c11cc954128eb430cb7882ad7c444dabd
AntAmeline <antonin.ameline@etu.minesparis.psl.eu>
2024-10-29 16:36:37 +0100

    df_cleared numérisé

--- common.py
+++ common.py
@@ -1,21 +1,21 @@
 #fichier contenant les données communes à tout le monde
 import pandas as pd 
 import geopandas as gpd 
-import numpy as np
-import matplotlib.pyplot as plt
 
 
-# +
 df_orig = pd.read_csv('world-country-electricity.csv')
 #sans valeur fausse
 df_cleared=df_orig.dropna(how='all')
-
+df_cleared=df_cleared.apply(pd.to_numeric, errors='coerce')
 #réorganisation
 df_byfeature=df_cleared.drop('Region', axis=1)
 df_byfeature=df_byfeature.set_index('Features')
 df_bycountry=df_cleared.drop('Region', axis=1)
 df_bycountry=df_bycountry.set_index('Country')
-df_bycountry.head(3)
-# -
+df_cleared.dtypes
+
+ #par région
+df_byregion=df_byregion.pivot_table(index=['Region','Features'],aggfunc='sum', fill_value=0)
+df_byregion
 
 
